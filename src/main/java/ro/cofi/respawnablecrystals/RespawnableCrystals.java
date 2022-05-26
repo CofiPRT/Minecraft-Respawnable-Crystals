f0228cab3a761f7245ff45fe19ee7c19021c41f3
CofiPRT <raresgsmart@gmail.com>
2022-05-26 12:44:18 +0300

    Initial commit

---
+++ src/main/java/ro/cofi/respawnablecrystals/RespawnableCrystals.java
@@ -0,0 +1,17 @@
+package ro.cofi.respawnablecrystals;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RespawnableCrystals extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
